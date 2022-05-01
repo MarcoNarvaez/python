ecd265877d38ecd15f032680d844d3afdda5253a
Marco Narvaez <67980783+MarcoNarvaez@users.noreply.github.com>
2022-05-01 16:05:52 -0300

    Add files via upload

---
+++ AdivinaElNro.py
@@ -0,0 +1,22 @@
+import random
+
+print('Hola, cual es tu nombre?')
+nombre = input()
+print('Bueno, ' + nombre + ', Estoy pensando en un numero entre el 1 y el 20')
+NumeroSecreto = random.randint(1, 20)
+
+for invitacion in range(1, 7):
+    print('Elige un numero')
+    invitado = int(input())
+
+    if invitado < NumeroSecreto:
+        print('Tu numero es muy bajo')
+    elif invitado > NumeroSecreto:
+        print('Tu numero es muy alto ')
+    else:
+        break #Numero correcto
+
+if invitado == NumeroSecreto:
+    print('Buen trabajo ' + nombre + ' Adivinaste el numero en ' + str(invitacion) + ' veces')
+else:
+    print('Nope, el numero que estaba pensando es: ' + str(NumeroSecreto))
