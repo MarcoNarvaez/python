73eecfe241c3c0fcd15799407cce663c537a8192
Marco Narvaez <67980783+MarcoNarvaez@users.noreply.github.com>
2022-05-01 20:04:47 -0300

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,14 @@
+# organizacion = 'udemy'
+
+# print('aprende a programar con ' + organizacion)
+# print('aprende a programar con {}'.format(organizacion))
+# print(f'aprende a programar con {organizacion}')
+
+adj = input('Adjetivo: ')
+verbo1 = input('Verbo: ')
+verbo2 = input('Verbo: ')
+sustantivo_prural = input('Sustantivo (prural): ')
+
+madlib = f'!Programar es tan {adj}! Me gusta demasiado porque me encanta {verbo1} problemas. Aprende a {verbo2} con freeCodeCamp y alcanza tus {sustantivo_prural}!'
+
+print(madlib)
